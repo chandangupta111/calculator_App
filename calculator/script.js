c24710956b1d011fa65685abc677edf191e8d9aa
Chandan Kumar <128067093+chandangupta111@users.noreply.github.com>
2023-08-23 20:34:07 -0700

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,23 @@
+// const display = document.getElementById("display")
+// const seven = document.getElementById("7")
+// const eight = document.getElementById("8")
+// function sevenes (){
+//     console.log(1)
+//     display.value=display.value+seven.value
+//     console.log(display.value)
+//     console.log(seven.value)
+// }
+
+// function eightes (){
+//     console.log(1)
+//     display.value=display.value+eight.value
+//     console.log(display.value)
+//     console.log(seven.value)
+// }
+
+// function equal (){
+
+//   display.value=  eval (display.value)
+//     console.log(display.value)
+//     console.log(eval (display.value))
+// }
